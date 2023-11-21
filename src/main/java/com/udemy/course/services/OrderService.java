9573b7ec2d601a62fa4f1067b9e15e45d28d1969
Emerson SL Melo <emersonlima910@gmail.com>
2023-11-21 20:22:00 -0300

    FIX: correcao mapeamento da classe Order

--- src/main/java/com/udemy/course/services/OrderService.java
+++ src/main/java/com/udemy/course/services/OrderService.java
@@ -22,8 +22,8 @@ public class OrderService {
     }
 
     public Order findById(Long id){
-      Optional<Order> obj =  orderRepository.findById(id);
-      return obj.get();
+        Optional<Order> obj =  orderRepository.findById(id);
+        return obj.orElse(new Order());
 
     }
 }
